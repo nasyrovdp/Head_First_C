5f51e431f956a981f74a1633c018194ab29a681c
joystickus <joystickus@yandex.ru>
2021-11-19 17:55:37 +0300

    still trying to make it read the whole line

--- Chapter4_small_tools/3bermuda/bermuda_v2.c
+++ Chapter4_small_tools/3bermuda/bermuda_v2.c
@@ -5,9 +5,9 @@ int main() {
     float longitude;
     char info[80];
     char line[300];
-    while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3) {
+    while (scanf("%299[^\n]\n", line) == 1) {
+        scanf("%f,%f,%179[^\n]", &latitude, &longitude, info);
         if ((latitude > 26) && (latitude < 34) && (longitude > -76) && (longitude < -64)) {
-            scanf("%299s", line);
             printf("%s\n", line);
         }
     }
